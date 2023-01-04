1ea80ad0266d966209333c75a351f81761728240
Alfonso Monroy <alfonso.m@cogentibs.com>
2023-01-03 19:49:29 -0600

    adding project navbar icon assets

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -3,10 +3,12 @@ import { LinkStyled } from "./link-styled";
 export const NavBar = () => {
 
   return (
-    <div className="flex items-center justify-center h-20 mx-16 rounded-b-3xl text-lg text-white capitalize space-x-11 bg-black">
+    <div className="container bg-black rounded-b-3xl max-w-7xl mx-auto px-6 py-5 md:px-0">
+    <div className="flex items-center justify-between text-lg text-white capitalize">
       <LinkStyled text="Home" href="/" />
       <LinkStyled text="Design" href="/design" />
       <LinkStyled text="Looking for talent" href="/looking-for-talent" />
+    </div>
     </div>
   )
 }
