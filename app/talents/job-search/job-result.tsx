0db1cbc99a41f256fc56cedcdff331439e7beef5
Natha-T <lenaat220@gmail.com>
2023-06-14 00:24:17 +0200

    feat: display job from db

--- app/talents/job-search/job-result.tsx
+++ app/talents/job-search/job-result.tsx
@@ -1,73 +1,46 @@
 "use client";
-
 import { Card } from "../../components/card";
+import { useEffect, useState } from "react";
 
-//TODO: ADD REAL DATA TO MAP THE CARD COMPONENT
 export default function JobResult() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    async function fetchJobs() {
+      try {
+        const response = await fetch("/api/talents/job-search");
+        const data = await response.json();
+        setJobs(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching job offers:", error);
+        // Handle error
+      }
+    }
+
+    fetchJobs();
+  }, []);
+
   return (
-    <>
-      <div className="grid min-w-full grid-flow-row grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-44xl:grid-cols-3">
+    <div className="grid min-w-full grid-flow-row grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-44xl:grid-cols-3">
+      {jobs.map((job) => (
         <Card
+          key={job.id} // Make sure to provide a unique key for each item
           type="company"
-          title="Job Title 3"
-          postedBy="by Goodhive"
-          postedOn="posted 2 days ago"
-          image="/img/company_img.png"
-          countryFlag="/img/country_flag.png"
-          city=""
-          rate=""
-          currency=""
-          description="Job description will come here when posted Amet, consecq consectetur consectetur adipiscing elit, sed do eiusmod tempor."
-          skills={["Skill 1", "Skill 2", "Skill 3", "Skill 4"]}
+          title={job.title}
+          postedBy={`by ${job.postedBy}`}
+          postedOn={`posted ${job.postedOn}`}
+          image={job.image}
+          countryFlag={job.countryFlag}
+          city={job.city}
+          rate={job.rate}
+          currency={job.currency}
+          description={job.description}
+          skills={job.skills}
           buttonText="Apply"
-          escrowFee="500"
-        />{" "}
-        <Card
-          type="company"
-          title="Job Title 3"
-          postedBy="by Goodhive"
-          postedOn="posted 2 days ago"
-          image="/img/company_img.png"
-          countryFlag="/img/country_flag.png"
-          city=""
-          rate=""
-          currency=""
-          description="Job description will come here when posted Amet, consecq consectetur consectetur adipiscing elit, sed do eiusmod tempor."
-          skills={["Skill 1", "Skill 2", "Skill 3", "Skill 4"]}
-          buttonText="Apply"
-          escrowFee="500"
-        />{" "}
-        <Card
-          type="company"
-          title="Job Title 3"
-          postedBy="by Goodhive"
-          postedOn="posted 2 days ago"
-          image="/img/company_img.png"
-          countryFlag="/img/country_flag.png"
-          city=""
-          rate=""
-          currency=""
-          description="Job description will come here when posted Amet, consecq consectetur consectetur adipiscing elit, sed do eiusmod tempor."
-          skills={["Skill 1", "Skill 2", "Skill 3", "Skill 4"]}
-          buttonText="Apply"
-          escrowFee="500"
-        />{" "}
-        <Card
-          type="company"
-          title="Job Title 3"
-          postedBy="by Goodhive"
-          postedOn="posted 2 days ago"
-          image="/img/company_img.png"
-          countryFlag="/img/country_flag.png"
-          city=""
-          rate=""
-          currency=""
-          description="Job description will come here when posted Amet, consecq consectetur consectetur adipiscing elit, sed do eiusmod tempor."
-          skills={["Skill 1", "Skill 2", "Skill 3", "Skill 4"]}
-          buttonText="Apply"
-          escrowFee="500"
+          escrowFee={job.escrowFee}
         />
-      </div>
-    </>
+      ))}
+    </div>
   );
 }
